d394d10c5055a7da22b5d1776e157d597dd756d6
Yatten <jonel.mawirat@bugomi.com>
2017-05-06 08:12:24 +0000

    config1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-test
-
-second
-
-third
-
-fourth
-
-fifth
+import pexpect
+child=pexpect.spawn("telnet 13.115.247.170 32773")
+child.sendline("\r\n")
+child.expect(">")
+child.sendline("enable")
+child.expect("#")
+child.sendline("hostname Router1")
+child.expect("#")
+child.sendline("end")
+child.expect("#")
